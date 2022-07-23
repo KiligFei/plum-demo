c70a73abcac8f137a99d783abc6e507822e4a21a
Kilig <1775814844@qq.com>
2022-07-23 22:23:08 +0800

    style: :art:

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
+import '@unocss/reset/tailwind.css'
+import 'uno.css'
+import routes from 'virtual:generated-pages'
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import routes from 'virtual:generated-pages'
 import App from './App.vue'
-
-import '@unocss/reset/tailwind.css'
 import './styles/main.css'
-import 'uno.css'
 
 const app = createApp(App)
 const router = createRouter({
